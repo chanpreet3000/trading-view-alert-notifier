55a489c9435328e3705f2dd2a21b568d4ee043df
Chanpreet Singh <chanpreet3000@gmail.com>
2024-09-19 16:32:43 +0530

    changing pop over color for inactive

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
     const currentUrl = tabs[0].url;
+    const body = document.body;
     const statusElement = document.getElementById('status');
     const redirectElement = document.getElementById('redirect');
     const redirectButton = document.getElementById('redirectButton');
@@ -8,6 +9,8 @@ document.addEventListener('DOMContentLoaded', function () {
     if (currentUrl.match(/https:\/\/.*\.tradingview\.com\/chart\/.*/)) {
       statusElement.innerHTML = '<div>Extension is active. Please visit any chart to start monitoring for alerts.</div>';
       redirectElement.style.display = 'none';
+      body.className = 'success';
+
     } else {
       statusElement.innerHTML = '<div class="text-red">Extension is NOT ACTIVE on this page.</div>';
       redirectElement.style.display = 'block';
